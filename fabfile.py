bb8506feb44eaa0b38a3d38956bf85c49f54bc5a
Christopher Peplin <peplin@bueda.com>
2010-10-03 23:05:25 -0400

    Switch to nose test runners - probably shouldn't use fabric in this project.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import os
 from fabric.api import *
-from fab_shared import (test, tornado_test_runner, tornado_deploy as deploy,
+from fab_shared import (test, nose_test_runner, webpy_deploy as deploy,
         setup, development, production, localhost, staging, restart_webserver,
-        rollback, lint, enable,
-        disable, maintenancemode, rechef)
+        rollback, lint, enable, disable, maintenancemode, rechef)
 
 env.unit = "trinity"
 env.path = "/var/tornado/%(unit)s" % env
@@ -14,7 +13,7 @@ env.root_dir = os.path.abspath(os.path.dirname(__file__))
 env.pip_requirements = ["requirements/common.txt",]
 env.pip_requirements_dev = ["requirements/dev.txt",]
 env.pip_requirements_production = ["requirements/production.txt",]
-env.test_runner = tornado_test_runner
+env.test_runner = nose_test_runner
 env.campfire_subdomain = 'bueda'
 env.campfire_room = 'Development'
 env.campfire_token = '63768eee94d96b7b18e2091f3919b2a2a3dcd12a'
