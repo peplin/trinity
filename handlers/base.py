76c41ec51714064f190546c0af72014c5a976b0f
Vasco Pedro <vasco@bueda.com>
2010-10-20 16:55:30 -0400

    added JPype Json Parser

--- handlers/base.py
+++ handlers/base.py
@@ -4,6 +4,30 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+import simplejson
+import datetime
+
+from tornado import escape
+
+
+class JPypeJSONEncoder(simplejson.JSONEncoder):
+    """
+    JSONEncoder subclass that knows how to encode JPype
+    """
+    def default(self, o):
+        try:
+            return super(JPypeJSONEncoder, self).default(o)
+        except TypeError:
+            try:
+                if isinstance(o,dict):
+                    return dict([ (k, super(JPypeJSONEncoder, self).default(v)) for k, v in data.iteritems() ])
+                elif isinstance(o, (tuple, list, set)):
+                    return [ super(JPypeJSONEncoder, self).default(v) for v in data ]
+            #if it is a primitive of type int
+            except TypeError:
+                print type(o)
+                return int(unicode(o))
+
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def graph(self):
@@ -39,6 +63,34 @@ class BaseHandler(tornado.web.RequestHandler):
         arg = self.request.arguments[name]
         return arg
 
-    def write(self):
+
+    def _utf8(self,s):
+        if isinstance(s, unicode):
+            return s.encode("utf-8")
+        assert isinstance(s, str)
+        return s
+
+
+    def write(self, chunk):
         # TODO override to handle JPype
+        """Writes the given chunk to the output buffer.
+
+        To write the output to the network, use the flush() method below.
+
+        If the given chunk is a dictionary, we write it as JSON and set
+        the Content-Type of the response to be text/javascript.
+
+        Note that lists are not converted to JSON because of a potential
+        cross-site security vulnerability.  All JSON output should be
+        wrapped in a dictionary.  More details at
+        http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
+        """
+        assert not self._finished
+        if isinstance(chunk, dict):
+            chunk = simplejson.dumps(chunk, cls=JPypeJSONEncoder, indent=4)
+            #chunk = simplejson.dumps(chunk)
+            #chunk = escape.json_encode(chunk)
+            self.set_header("Content-Type", "text/javascript; charset=UTF-8")
+        chunk = self._utf8(chunk)
+        self._write_buffer.append(chunk)
         
