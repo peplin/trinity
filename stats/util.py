bdebd006b1e5cfe92c299b02f954e1136026a78e
Ken Kochis <ken@bueda.com>
2010-10-22 16:41:23 -0400

    Call yapi(search_term) to get number of documents for seach_team

---
+++ stats/util.py
@@ -0,0 +1,13 @@
+import urllib, urllib2
+import simplejson
+
+APPID = "RzRkxHvV34Ej1d2DFl.je2afuqgpGxqDh3DW0EMLjl_zUl7NXxmlot.SkPXGPk3I6g--"
+SEARCH_URL = "http://boss.yahooapis.com/ysearch/web/v1/"
+
+
+def yapi(term):
+    params = {'appid': APPID, 'format': 'json'}
+    url = "%s%s?%s" % (SEARCH_URL, urllib.quote(term), urllib.urlencode(params))
+    result = simplejson.loads(urllib2.urlopen(url).read())
+    return result['ysearchresponse']['totalhits']
+
