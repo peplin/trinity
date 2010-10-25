9370cae256298375ea262c32a10aa9cd0fb94f61
Ken Kochis <ken@bueda.com>
2010-10-25 17:00:56 -0400

    Use pythong logging, remove commonware

--- stats/random_walk.py
+++ stats/random_walk.py
@@ -5,8 +5,8 @@ import networkx as nx
 from util import yapi
 from collections import deque
 
-import commonware.log
-logger = commonware.log.getLogger('trinity.stats,random_walk')
+import logging
+logging.getLogger(__name__)
 
 
 DEFAULT_DEPTH = 5
@@ -96,7 +96,7 @@ def random_walk(graph, nodeid, depth=DEFAULT_DEPTH):
     return path
 
 def get_user_topics(graph, node):
-    logger.debug(u'getting topics for %s' % node)
+    logging.debug(u'getting topics for %s' % node)
     P = {} # dictionary of predecessors
     C = {} # dictionary of counts
     
@@ -137,19 +137,19 @@ def get_topics(graph, node):
     nxgraph = None
     nodeid = node.id
     
-    logger.debug(u'executing networkx graph transformation')
+    logging.debug(u'executing networkx graph transformation')
     with graph.transaction:
         nxgraph = get_nx_graph(graph, node, maxpath=max_topics)
 
     if nxgraph:
         user_counts, user_pred = get_user_topics(nxgraph, nodeid)
-        logger.debug(u'normalizing counts')
+        logging.debug(u'normalizing counts')
         counts = normalize_counts(nxgraph, user_counts)
         sorted_counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse= True)
         if len(sorted_counts) < max_topics:
             max_topics = len(sorted_counts)
 
-        logger.debug(u'generating subtopics')
+        logging.debug(u'generating subtopics')
         for topic, count in sorted_counts[:max_topics]:            
             topics.append(get_subtopics_recursive(nxgraph, topic, user_pred, counts, nodeid))
             
